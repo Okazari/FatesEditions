6cffa9456389279e3a9af6002e8d7eb60842e1ee
Okazari <plouzennec.benjamin@gmail.com>
2015-02-26 08:28:25 +0000

    Finalisation du menu latéral

--- app/feature/game/controller/GameController.js
+++ app/feature/game/controller/GameController.js
@@ -4,7 +4,4 @@ angular.module('myVirtualStoryBookApp')
   .controller('GameController', function ($scope, GameService) {
     $scope.displayMenu = false;
     $scope.currentGame = GameService.getGame(1);
-    $scope.toggleRightMenu = function(){
-      $scope.displayMenu = !$scope.displayMenu;
-    }
   });
