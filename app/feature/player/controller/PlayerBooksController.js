b12f66c52ad041ff37f5d63554937e9b5ea67684
Okazari <plouzennec.benjamin@gmail.com>
2015-02-27 15:59:41 +0000

    On avance vers un truc propre visuellement

--- app/feature/player/controller/PlayerBooksController.js
+++ app/feature/player/controller/PlayerBooksController.js
@@ -4,8 +4,15 @@ angular.module('myVirtualStoryBookApp')
   .controller('PlayerBooksController', function ($scope, PlayerBooksService) {
     $scope.books = PlayerBooksService.getPublishedBook();
     $scope.displayedBook = $scope.books[0];
+    $scope.genreIconMap = {"romance" : "glyphicon-heart",
+                           "rpg": "glyphicon-tower",
+                           "aventure": "glyphicon-road",
+                           "action": "glyphicon-fire"
+                          };
     $scope.selectBook = function(book){
       $scope.displayedBook = book;
     }
-    
+    $scope.getGenreIcon = function(genre){
+      return $scope.genreIconMap[genre.toLowerCase()];
+    }
   });
