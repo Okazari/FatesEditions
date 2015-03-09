9a5e0155b4c2f6a9e40331928d97ff1018ad3887
Okazari <plouzennec.benjamin@gmail.com>
2015-03-09 16:33:44 +0000

    Finalisation de la page de jeu

--- app/feature/game/controller/GameController.js
+++ app/feature/game/controller/GameController.js
@@ -1,7 +1,13 @@
 'use strict';
 
 angular.module('myVirtualStoryBookApp')
-  .controller('GameController', function ($scope, $stateParams, GameService) {
+  .controller('GameController', function ($scope, $stateParams, GameService, GamePageService) {
     $scope.displayMenu = false;
     $scope.currentGame = GameService.getGame($stateParams.id);
+    $scope.currentPage = GamePageService.getPage($scope.currentGame.currentPage);
+    
+    $scope.changePage = function(newPageId){
+      $scope.currentPage = GamePageService.getPage(newPageId);
+      $scope.currentGame.currentPage = newPageId;
+    }
   });
