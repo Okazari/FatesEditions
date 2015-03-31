ffbf57800fc00cc3afa2477a30f68a04336690b7
Okazari <plouzennec.benjamin@gmail.com>
2015-03-31 12:53:30 +0000

    Ajout de la gestion d'image dans les pages

--- symfony/src/MVSB/MyVirtualStoryBookBundle/Service/PageService.php
+++ symfony/src/MVSB/MyVirtualStoryBookBundle/Service/PageService.php
@@ -30,6 +30,7 @@ class PageService{
         $page->setTitle($pageFromJson->getTitle());
         $page->setDescription($pageFromJson->getDescription());
         $page->setText($pageFromJson->getText());
+        $page->setImage($pageFromJson->getImage());
         $transitions = [];
         $page->setTransitions($transitions);
         forEach($pageFromJson->getTransitions() as $transitionFromJson){
