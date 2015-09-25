7d447a97dabfab57e64eeb07d78da7fa67ac4941
Okazari <plouzennec.benjamin@gmail.com>
2015-09-25 15:54:54 +0000

    Début de la configuation du Tour

--- public/angularApp/app/feature/write/controller/WriteBookController.js
+++ public/angularApp/app/feature/write/controller/WriteBookController.js
@@ -54,4 +54,67 @@ angular.module('myVirtualStoryBookApp')
         BookService.getBookPages($stateParams.id).success($scope.updatePages)
       });
     }
+    
+    
+    $scope.launchTour = function(){
+      // Instance the tour
+      $scope.tour = new Tour({
+        steps: [
+        {
+          element: "#book-title",
+          title: "Titre du livre",
+          content: "Entre ici le titre de ton livre",
+          placement: "top",
+        },
+        {
+          element: "#book-genre",
+          title: "Genre du livre",
+          content: "Selectionne un genre pour aider les joueurs à trouver ton livre",
+          placement: "top",
+        },
+        {
+          element: "#book-synopsis",
+          title: "Synospsis du livre",
+          content: "Résume rapidement ton livre et donne envie aux joueurs de le lire",
+          placement: "top",
+        },
+        {
+          element: "#book-cover",
+          title: "Image de couverture",
+          content: "Ajoute une image qui servira de couverture à ton live. Copie colle simplement le lien vers l'image",
+          placement: "bottom",
+        },
+        {
+          element: "#book-starting-page",
+          title: "Page de départ",
+          content: "Pense à préciser la page de départ de ton livre",
+          placement: "top",
+        },
+        {
+          element: "#book-stats",
+          title: "Statistiques du livre",
+          content: "Ajoute autant de statistiques que tu veux à ton livre, elle te permetrons de conditionner l'affichage des choix du joueur",
+          placement: "top",
+        },],
+        container: "body",
+        keyboard: true,
+        storage: window.localStorage,
+        debug: false,
+        backdrop: true,
+        backdropContainer: 'main-wrapper',
+        backdropPadding: 10,
+        redirect: true,
+        orphan: true
+      });
+      
+      // Initialize the tour
+      $scope.tour.init(true);
+      
+      // Start the tour
+      $scope.tour.start();
+      console.log($scope.tour);
+    }
+    
+    $scope.launchTour();
+    
 });
