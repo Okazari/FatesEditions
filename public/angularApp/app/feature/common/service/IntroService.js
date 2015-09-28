fc8d5110e87e66f284da01e44a1f066dbe78db57
Okazari <plouzennec.benjamin@gmail.com>
2015-09-28 14:59:18 +0000

    Corrections de formulation et d'orthographe

--- public/angularApp/app/feature/common/service/IntroService.js
+++ public/angularApp/app/feature/common/service/IntroService.js
@@ -23,79 +23,79 @@ myVirtualStoryBookApp.service("IntroService",
             "writebooktour":[
                 {
                   element: "#book-title",
-                  intro: "Entre ici le titre de ton livre",
+                  intro: "Ajoute un titre",
                   position: top,
                 },
                 {
                   element: "#book-genre",
-                  intro: "Selectionne ici un genre pour aider les joueurs à trouver ton livre",
+                  intro: "Selectionne un genre",
                   position: top,
                 },
                 {
                   element: "#book-synopsis",
-                  intro: "Résume ici rapidement ton livre et donne envie aux joueurs de le lire",
+                  intro: "Résume rapidement ton livre",
                   position: top,
                 },
                 {
                   element: "#book-cover",
-                  intro: "<p>Ajoute ici une image qui servira de couverture à ton live.</p> Copie colle simplement le lien vers l'image",
+                  intro: "<p>Ajoute une image qui servira de couverture à ton live.</p> Copie colle simplement le lien vers l'image",
                   position: right,
                 },
                 {
                   element: "#book-starting-page",
-                  intro: "Précise ici la page de départ de ton livre",
+                  intro: "Précise la page de départ",
                   position: top,
                 },
                 {
                   element: "#book-stats",
-                  intro: "Ajoute ici autant de statistiques que tu veux à ton livre, elle te permetrons de conditionner l'affichage des choix du joueur",
+                  intro: "<p>Ajoute des statistiques de personnages, elles te permetront de conditionner l'affichage des choix proposés au joueur</p> A la création du livre, les statistiques du joueur sont initialisées à la valeur saisie dans le champ 'Valeur initiale'",
                   position: top,
                 },
                 {
                   element: "#book-objects",
-                  intro: "Ajoute ici tous les objets que le joueur pourra obtenir au cours de son aventure",
+                  intro: "<p>Ajoute des objets que le joueur pourra obtenir au cours de son aventure</p><p>Si l'objet doit être présent dans l'inventaire dès le début de l'aventure, coche la case 'début' de l'objet en question.</p>",
                   position: top,
                 },
                 {
                   element: "#book-graph",
-                  intro: "Ce graph illustrera les différents chemins de ton livre lorsque tu aura lié des pages via des transitions",
+                  intro: "Ce graphique illustrera les différents chemins de ton livre",
                   position: right,
                 },
                 {
                   element: "#book-pages",
-                  intro: "Ajoute ici de nouvelles pages à ton livre",
+                  intro: "Ajoute de nouvelles pages à ton livre",
                   position: left,
                 },
             ],
             "writepagetour":[
                 {
                   element: "#page-title",
-                  intro: "<p>Entre ici le titre de la page, attention ce titre sera visible des joueurs à certains endroit</p>",
+                  intro: "<p>Ajoute un titre à ta page, attention il sera visible des joueurs</p>",
                   position: top,
                 },
                 {
                   element: "#page-memo",
-                  intro: "<p>Ecris ici un rapide mémo pour te souvenir de ce qu'il se passe dans la page, ce mémo n'est jamais visible des joueurs.</p> Le mémo t'aidera à retrouver ta page dans la liste des pages du livre",
+                  intro: "<p>Résume en quelques mots le déroulement de la page, ce mémo n'est jamais visible des joueurs.</p> Le mémo t'aidera à retrouver ta page dans la liste des pages du livre",
                   position: left,
                 },
                 {
                   element: "#page-effects",
-                  intro: "Tu peux ajouter des effets qui modifierons l'inventaire ou les statistiques du joueurs lorsqu'il tombera sur cette page",
+                  intro: "Tu peux ajouter des effets qui modifieront l'inventaire et/ou les statistiques du joueur lorsqu'il atteindra cette page",
                   position: top,
                 },
                 {
                   element: "#page-background-music",
-                  intro: "<p>Vous pouvez rajouter une musique de fond qui se lancera à l'arrivée sur la page et continuera jusqu'à ce qu'une autre page change la musique de fond</p> Direction <a href='https://soundcloud.com/'>https://soundcloud.com/</a>, recherchez et choisissez une musique puis copiez-collez l'url dans le champ ci-dessous.",
+                  intro: "<p>Tu peux ajouter une musique de fond qui se lancera à l'arrivée sur la page et continuera jusqu'à ce qu'une autre page la change</p> Direction <a href='https://soundcloud.com/'>https://soundcloud.com/</a>, recherche et choisis une musique puis copie-colle l'url dans le champ ci-dessous.",
                   position: top,
                 },
                 {
                   element: "#page-content",
-                  intro: "<p>Ecrivez ici le texte de votre page. N'hésitez pas à utiliser l'éditeur pour formater votre texte à votre guise.</p> Vous pouvez aussi ajouter des images ou vidéos youtubes",
+                  intro: "<p>Ajoute le texte de ta page. N'hésitez pas à utiliser l'éditeur pour formater ton texte à ta guise.</p> Tu peux aussi ajouter des images ou des vidéos youtube",
                   position: top,
                 },
                 {
                   element: "#page-transitions",
-                  intro: "<p>Vous pouvez ici définir les transitions de votre page. Ces transitions représente les choix qui seront proposé aux joueurs.</p><p>Vous pouvez leur définir des conditions d'affichages (en fonction des objets et statistiques du joueurs à son arrivée sur la page</p><p>Vous pouvez définir des effets qui s'appliquerons si le joueur choisit la transition</p>",
+                  intro: "<p>Ajoute des transitions à ta page. Ces transitions représentent les choix qui seront proposés aux joueurs.</p><p>Tu peux définir des conditions d'affichages (en fonction des objets et statistiques du joueurs à son arrivée sur la page)</p><p>Tu peux aussi définir des effets qui s'appliqueront si le joueur choisit la transition</p>",
                   position: top,
                 }
             ],
@@ -103,12 +103,12 @@ myVirtualStoryBookApp.service("IntroService",
                 {
                   intro: "<p style='width:100%;text-align:center'><b>MyVirtualStoryBook</b></p>"+
                          "<p>Bienvenue sur la version Béta de MyVirtualStoryBook</p>"+
-                         "<p>Cette application te permet de jouer et d'écrire des livres-jeux</p>",
+                         "<p>Cette application te permets de jouer et d'écrire des livres-jeux</p>",
                          position: top,
                 },
                 {
-                  intro: "<p>Différents tutoriels t'accompagnerons dans la compréhension de l'application, il est recommandé de les suivre jusqu'au bout.</p>"+
-                         "<p>Dans le cas ou tu préfère découvrir par toi même, tu peux directement cliquer sur passer. Sinon clique sur suivant pour continuer</p>",
+                  intro: "<p>Différents tutoriels t'accompagneront dans la compréhension de l'application, il est recommandé de les suivre jusqu'au bout.</p>"+
+                         "<p>Dans le cas ou tu préfère découvrir par toi même, tu peux directement cliquer sur passer. Sinon clique sur suivant pour continuer.</p>",
                          position: top,
                 },
                 {
@@ -118,12 +118,12 @@ myVirtualStoryBookApp.service("IntroService",
                 },
                 {
                   element: "#menu-choosegame",
-                  intro: "<p>Clique ici pour reprendre une partie que tu as laissé en pause</p>",
+                  intro: "<p>Clique ici pour reprendre une partie laissée en pause</p>",
                   position: right,
                 },
                 {
                   element: "#menu-createbook",
-                  intro: "<p>Clique ici pour commencer la création d'un nouveau livre </p>",
+                  intro: "<p>Clique ici pour commencer la création d'un nouveau livre</p>",
                   position: right,
                 },
                 {
