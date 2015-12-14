f4fe27f3910e56b9053f4c7197247703829370bb
Okazari <plouzennec.benjamin@gmail.com>
2015-12-14 11:10:07 +0000

    Amélioration du deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 # Indique au système que l'argument qui suit est le programme utilisé pour exécuter ce fichier
 # En règle générale, les "#" servent à mettre en commentaire le texte qui suit comme ici
 cd public/angularApp/
+npm install bower -g
 bower install
