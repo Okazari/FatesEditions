980c623e00b46455bac753b1693e65a45fdfe6f9
Okazari <plouzennec.benjamin@gmail.com>
2015-12-14 13:17:11 +0000

    Mise en place des migrations

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Indique au système que l'argument qui suit est le programme utilisé pour exécuter ce fichier
 # En règle générale, les "#" servent à mettre en commentaire le texte qui suit comme ici
-cd public/angularApp/
+
 npm install bower -g
+npm install mongoose-data-migrate -g
 bower install
+cd public/angularApp/
+cd ../../models/
+mongoose-data-migrate up
