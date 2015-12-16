eaf9637b9f72d5ed25d9b177ce57dd023393d952
Okazari <plouzennec.benjamin@gmail.com>
2015-12-16 10:57:28 +0000

    MAJ Syntaxique

--- routes/player.js
+++ routes/player.js
@@ -5,20 +5,19 @@ var Player = require('../models/PlayerModel');
 
 /************Players**********/
 router.get('/', function(req, res, next) {
-    Player.find({},"username",function(err, players) {
-            if (err)
-                next(err);
-
-            res.json(players);
-        });
+    Player.find({},"username").then(function(players) {
+        res.json(players);
+    },function(err){
+        next(err);
+    });
 }); 
 
 router.get('/:playerId', function(req, res, next) {
-    Player.findOne({_id:req.params.playerId},'username',function(err, player) {
-            if (err)
-                next(err);
-            res.json(player);
-        });
+    Player.findOne({_id:req.params.playerId},'username').then(function(player) {
+        res.json(player);
+    },function(err){
+        next(err);
+    });
 }); 
 
 router.post('/', function(req, res, next) {
@@ -27,9 +26,10 @@ router.post('/', function(req, res, next) {
     
     player.username = req.body.username;
     player.password = req.body.password;
-    player.save(function(err) {
-        if (err) next(err);
+    player.save().then(function() {
         res.json(player);
+    },function(err){
+        next(err);
     });
 }); 
 
