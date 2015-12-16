3daea2794fc78e79411e75669758de1a1f7c37d7
Okazari <plouzennec.benjamin@gmail.com>
2015-12-16 10:51:57 +0000

    MAJ Syntaxique

--- routes/page.js
+++ routes/page.js
@@ -6,19 +6,19 @@ var Transition = require('../models/TransitionModel');
 
 /************PAGES**********/
 router.get('/', function(req, res, next) {
-    Page.find(function(err, pages) {
-            if (err)
-                next(err);
-            res.json(pages);
-        });
+    Page.find().then(function(pages) {
+        res.json(pages);
+    },function(err){
+        next(err);
+    });
 });
 
 router.get('/:pageId', function(req, res, next) {
     if(req.params.pageId !== "undefined"){
-        Page.findOne({_id:req.params.pageId},function(err, page) {
-            if (err)
-                next(err);
+        Page.findOne({_id:req.params.pageId}).then(function(page) {
             res.json(page);
+        },function(err){
+            next(err);
         });
     }else{
         res.sendStatus(404);
@@ -29,20 +29,16 @@ router.get('/:pageId', function(req, res, next) {
 router.post('/', function(req, res, next) {
     var page = new Page();
     page.bookId = req.body.bookId;
-    page.save(function(err) {
-        if (err)
-            next(err);
-
+    page.save().then(function() {
         res.json(page);
+    },function(err){
+        next(err);
     });
 });
 
 
 router.patch('/:pageId', function(req, res, next) {
-    Page.findOne({"_id":req.params.pageId},function(err, page){
-        if(err){
-            next(err);  
-        }
+    Page.findOne({"_id":req.params.pageId},function(page){
         if(req.body.title) page.title = req.body.title;
         if(req.body.text) page.text = req.body.text;
         if(req.body.description) page.description = req.body.description;
@@ -50,35 +46,37 @@ router.patch('/:pageId', function(req, res, next) {
         if(req.body.bookId) page.bookId = req.body.bookId;
         if(req.body.effects) page.effects = req.body.effects;
         if(req.body.backgroundMusic) page.backgroundMusic = req.body.backgroundMusic;
-        page.save(function(err){
-            if(err)
-                next(err);
+        page.save().then(function(){
             res.json(page);
+        },function(err){
+            next(err);
         })
+    },function(err){
+        next(err);
     })
 });
 
 router.delete('/:pageId', function(req, res, next) {
-    Transition.remove({fromPage:req.params.pageId},function(err){
-        if(err) next(err);
-        Page.remove({_id:req.params.pageId},function(err){
-            if(err) next(err);
+    Transition.remove({fromPage:req.params.pageId}).then(function(){
+        Page.remove({_id:req.params.pageId}).then(function(){
             res.send(200);
+        },function(err){
+            next(err);
         })
+    },function(err){
+        next(err);
     });
 });
 
 router.get('/:pageId/transition', function(req, res, next) {
-    Page.findOne({"_id":req.params.pageId},function(err, page){
-        if(err){
-            next(err);  
-        }
-        Transition.find({"fromPage":req.params.pageId},function(err,transitions){
-            if(err){
-                next(err);
-            }
+    Page.findOne({"_id":req.params.pageId}).then(function(page){
+        Transition.find({"fromPage":req.params.pageId}).then(function(transitions){
             res.json(transitions);
+        },function(err){
+            next(err);
         })
+    },function(err){
+        next(err);
     })
 });
 
