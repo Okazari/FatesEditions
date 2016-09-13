442234fa84648d8406d10b09051e25cb913a419c
Okazari <plouzennec.benjamin@gmail.com>
2016-09-13 18:48:59 +0200

    Added portal title & app icon (stubed)

--- ui/src/components/Portal/Portal.js
+++ ui/src/components/Portal/Portal.js
@@ -4,7 +4,13 @@ import styles from './style.scss'
 const Portal = ({ children }) => {
   return (
     <div className={styles.component}>
-      {children}
+      <div className={styles.wrapper}>
+        <div className={styles.title}>
+          <img role="presentation" src="https://myvirtualstorybook.com/app/images/logo/logo300x300.png"/>
+          MyVirtualStoryBook
+        </div>
+        {children}
+      </div>
     </div>
   )
 }
