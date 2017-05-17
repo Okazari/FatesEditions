f5f184014399d7ff82ca716c59d6926f91db9fbb
Plouzennec Benjamin <plouzennec.benjamin@gmail.com>
2017-05-17 13:27:00 +0200

    Linter on services

--- ui/src/services/PageService.js
+++ ui/src/services/PageService.js
@@ -1,12 +1,14 @@
+// eslint-disable-next-line
 import { ResourceService } from 'react-rest-resource'
 import HttpService from './HttpService'
+
 export default new ResourceService('/api/page',
   {
     name: {
       single: 'page',
       plural: 'pages',
-      id: '_id'
+      id: '_id',
     },
     proxy: HttpService.fetch,
-  }
+  },
 )
