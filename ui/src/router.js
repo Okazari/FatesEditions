a8b6d5f8e54f10ff4525d9c8d95bf0d2e5bdad60
Plouzennec Benjamin <plouzennec.benjamin@gmail.com>
2017-05-17 13:30:45 +0200

    Level 1 linter

--- ui/src/router.js
+++ ui/src/router.js
@@ -1,26 +1,26 @@
 import React from 'react'
-import App from 'components/App'
-import Portal, { SignIn, SignUp, Recover } from 'components/Portal'
-import { PlayBooks, PlayGames } from 'components/Play'
-import { ShareBook, SharePublications } from 'components/Share'
-import { WriteBook, WriteDrafts, WritePage } from 'components/Write'
-import Profile from 'components/Profile'
 import { Router, Route, IndexRedirect, IndexRoute, browserHistory } from 'react-router'
+import App from './components/App'
+import Portal, { SignIn, SignUp, Recover } from './components/Portal'
+import { PlayBooks, PlayGames } from './components/Play'
+import { ShareBook, SharePublications } from './components/Share'
+import { WriteBook, WriteDrafts, WritePage } from './components/Write'
+import Profile from './components/Profile'
 
 const AppRouter = () => {
   return (
     <Router history={browserHistory}>
-      <Route path='/'>
+      <Route path="/">
         <IndexRedirect to="portal" />
         <Route path="portal" component={Portal}>
           <IndexRedirect to="signin" />
           <Route path="signin" component={SignIn} />
           <Route path="signup" component={SignUp} />
           <Route path="recover" component={Recover} />
-          </Route>
-          <Route path="app" component={App}>
-            <IndexRedirect to="play" />
-            <Route path="play" >
+        </Route>
+        <Route path="app" component={App}>
+          <IndexRedirect to="play" />
+          <Route path="play" >
             <IndexRedirect to="books" />
             <Route path="books" component={PlayBooks} />
             <Route path="Games" component={PlayGames} />
