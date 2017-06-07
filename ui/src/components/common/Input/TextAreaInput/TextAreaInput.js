c9601f0c336a0e003361fec809f5f3c251d4eb9a
Mahdi Aici <aici.mahdi@gmail.com>
2017-06-07 17:37:30 +0200

    TextAreaInput refactored

--- ui/src/components/common/Input/TextAreaInput/TextAreaInput.js
+++ ui/src/components/common/Input/TextAreaInput/TextAreaInput.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import styles from './styles.scss'
 
-const TextAreaInput = ({ injectedProps, domProps, label }) => {
+const TextAreaInput = ({ domProps, label }) => {
   return (
     <div className={styles.component}>
       <label htmlFor={domProps.id}>{label}</label>
       <textarea
-        value={injectedProps.inputValue}
-        onChange={injectedProps.updateInput}
         {...domProps}
       />
     </div>
