735f523becac02bdfaf578473a80f5d4067d7075
Mahdi Aici <aici.mahdi@gmail.com>
2017-06-07 17:38:01 +0200

    Inputs refactoring

--- ui/src/components/common/Input/LabelInput/LabelInputContainer.js
+++
@@ -1,4 +0,0 @@
-import { InputHoc as inputHoc } from '../InputHoc'
-import LabelInput from './LabelInput'
-
-export default inputHoc(LabelInput)
--- ui/src/components/common/Input/RowInput/RowInputContainer.js
+++
@@ -1,4 +0,0 @@
-import RowInput from './RowInput'
-import { InputHoc as inputHoc } from '../InputHoc'
-
-export default inputHoc(RowInput)
