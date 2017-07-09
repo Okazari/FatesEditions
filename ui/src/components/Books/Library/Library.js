ad6bdd7529a53407c8cbe36b62d2c985390f1a91
Okazari <plouzennec.benjamin@gmail.com>
2017-07-09 17:44:44 +0200

    Improved show up animation

--- ui/src/components/Books/Library/Library.js
+++ ui/src/components/Books/Library/Library.js
@@ -11,20 +11,24 @@ class Library extends React.Component {
 
   render() {
     const { drafts } = this.props
-
+    const nbColumns = document && Math.floor((document.body.clientWidth - 200) / 240)
+    console.log(nbColumns)
     // TODO Replace with loader
     if (!drafts) return null
     return (
-      <div className={styles.component}>
+      <div className={styles.component} ref={library => this.library = library}>
         {
-          drafts.map((book, index) => (
-            <div
-              key={book}
-              className={styles.book}
-            >
-              <Book showDelay={100 * (index + 1)} draftId={book} />
-            </div>
-          ))
+          drafts.map((book, index) => {
+            const delay = 50 * ((index % nbColumns) + Math.floor(index / nbColumns) + 1)
+            return (
+              <div
+                key={book}
+                className={styles.book}
+              >
+                <Book showDelay={delay} draftId={book} />
+              </div>
+            )
+          })
         }
       </div>
     )
