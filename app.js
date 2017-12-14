0126c1722884b92cb49679bbaeccd35c465a519d
Plouzennec Benjamin <plouzennec.benjamin@gmail.com>
2017-12-14 12:10:59 +0100

    Hotfix ?

--- app.js
+++ app.js
@@ -40,25 +40,25 @@ var jwt = require('jsonwebtoken');
 
 app.use('/api',portal);
 
-app.use(function(req, res, next){
-  if(!req.get('Authorization')){
-    var err = new Error('Not Authorized');
-    err.status = 401;
-    next(err);
-  }else{
-    try{
-      var payload = jwt.verify(req.get('Authorization'),'mysecretstory');
-      req.payload = payload;
-      var token = jwt.sign({user:payload.user},"mysecretstory",{expiresIn:3600});
-      console.log(payload.user.username+' '+payload.exp);
-      res.set('Auth-token', token);
-      next();
-    }catch(err){
-      err.status = 401;
-      next(err);
-    }
-  }
-})
+// app.use(function(req, res, next){
+//   if(!req.get('Authorization')){
+//     var err = new Error('Not Authorized');
+//     err.status = 401;
+//     next(err);
+//   }else{
+//     try{
+//       var payload = jwt.verify(req.get('Authorization'),'mysecretstory');
+//       req.payload = payload;
+//       var token = jwt.sign({user:payload.user},"mysecretstory",{expiresIn:3600});
+//       console.log(payload.user.username+' '+payload.exp);
+//       res.set('Auth-token', token);
+//       next();
+//     }catch(err){
+//       err.status = 401;
+//       next(err);
+//     }
+//   }
+// })
 
 /******REST ROUTES*******/
 app.use('/api/player',player);
