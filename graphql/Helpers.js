3906299e79818c9bac0e6587951f72f3a287e238
Plouzennec Benjamin <plouzennec.benjamin@gmail.com>
2018-01-09 14:33:04 +0100

    Removed console.log

--- graphql/Helpers.js
+++ graphql/Helpers.js
@@ -1,7 +1,5 @@
 const getProjection =  (fieldASTs) => {
-  console.log("YOLOOO",fieldASTs.fieldNodes[0].selectionSet)
   return fieldASTs.fieldNodes[0].selectionSet.selections.reduce((projections, selection) => {
-    console.log("WEHS", selection.name.value)
     projections[selection.name.value] = true;
     return projections;
   }, {})
