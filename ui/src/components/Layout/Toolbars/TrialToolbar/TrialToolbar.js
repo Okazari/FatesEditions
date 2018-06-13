9376c5737627c86f672ed2ce396c517adfec481e
RQM- <benoit.jacques.felix@gmail.com>
2018-06-13 14:02:51 +0200

    :sparkles::art: add DisplayObjects to TrialToolbar too

--- ui/src/components/Layout/Toolbars/TrialToolbar/TrialToolbar.js
+++ ui/src/components/Layout/Toolbars/TrialToolbar/TrialToolbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { browserHistory } from 'react-router'
 import { ToolbarButton } from 'components/common/ToolbarLink'
 import DisplayStats from './DisplayStats'
+import DisplayObjects from './DisplayObjects'
 import styles from './style.scss'
 import logoWhite from '../../../common/logo_white.svg'
 // import { RouteService } from '../../../../services'
@@ -12,6 +13,7 @@ const TrialToolbar = ({ game }) => {
       <div className={styles.top}>
         <img className={styles.logo} alt="logo" src={logoWhite} />
         <DisplayStats game={game} />
+        <DisplayObjects game={game} />
       </div>
       <div className={styles.bottom}>
         {/* TODO: Externalize this button */}
