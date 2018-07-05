fbd0feb0b5df0c8ef26bfea50a405a705d9a72ef
RQM- <benoit.jacques.felix@gmail.com>
2018-07-05 14:57:27 +0200

    💄 add styling on Errors throwed

--- ui/src/components/Game/GamePage/GameTransition/GameTransition.js
+++ ui/src/components/Game/GamePage/GameTransition/GameTransition.js
@@ -4,64 +4,28 @@ import GameService from 'services/GameService'
 import Button from 'components/common/Button'
 import styles from './styles.scss'
 
-//  input: {
-//  visible: boolean,
-//  errors: [String]
-//  transitionId,
-//  text: String,
-//  }
-
-// Move checkTransitionVisibility to HOC
-
 const GameTransition = ({
   visible,
   errors,
   transitionId,
   text,
 }) => {
-  
+  const className = classnames(styles.component, {
+    [styles.disabled]: errors.length > 0,
+  })
   if (!visible) return null 
   return (
     <Button 
       domProps={{
         onClick: () => !!transition.toPage && GameService.changePageAndApplyEffects(transitionId),
       }}
-      className={styles.component}
+      className={className}
     >
       {text}
-      {errors.map(error => console.log(error) || <div className={styles.error}>{error.message}</div>)}
+      {errors.map(error => <div className={styles.error}>{error.message}</div>)}
     </Button>
   )
 }
-
-// OLDSCHOOL
-
-// const GameTransition = ({
-//   transition,
-//   effects,
-// }) => {
-//   const { isVisible, incompleteCondition } = GameService.checkTransitionVisibility(transition)
-//   if (!isVisible) return null
-
-//   const onClick = () =>
-//     transition.toPage && GameService.changePageAndApplyEffects(transition.toPage, effects)
-//   const className = classnames(styles.component, {
-//    [styles.disabled]: transition.toPage === null || incompleteCondition,
-//   })
-
-//   return (
-//     <Button
-//       domProps={{
-//         onClick,
-//       }}
-//       className={className}
-//     >
-//       {transition.text}
-//       {!transition.toPage && <div>Page de destination manquante</div>}
-//       {!!incompleteCondition && <div>Condition incomplète</div>}
-//     </Button>
-//   )
-// }
 
 export default GameTransition
 
