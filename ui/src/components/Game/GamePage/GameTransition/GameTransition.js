5ab43f41e47d2ec79dfef23fac1efd0927a69755
RQM- <benoit.jacques.felix@gmail.com>
2018-07-05 14:49:07 +0200

    👌 now throw Error and catching them in the ReduxContainer

--- ui/src/components/Game/GamePage/GameTransition/GameTransition.js
+++ ui/src/components/Game/GamePage/GameTransition/GameTransition.js
@@ -4,32 +4,64 @@ import GameService from 'services/GameService'
 import Button from 'components/common/Button'
 import styles from './styles.scss'
 
+//  input: {
+//  visible: boolean,
+//  errors: [String]
+//  transitionId,
+//  text: String,
+//  }
+
+// Move checkTransitionVisibility to HOC
+
 const GameTransition = ({
-  transition,
-  effects,
+  visible,
+  errors,
+  transitionId,
+  text,
 }) => {
-  const { isVisible, incompleteCondition } = GameService.checkTransitionVisibility(transition)
-  if (!isVisible) return null
-
-  const onClick = () =>
-    transition.toPage && GameService.changePageAndApplyEffects(transition.toPage, effects)
-  const className = classnames(styles.component, {
-    [styles.disabled]: transition.toPage === null || incompleteCondition,
-  })
-
+  
+  if (!visible) return null 
   return (
-    <Button
+    <Button 
       domProps={{
-        onClick,
+        onClick: () => !!transition.toPage && GameService.changePageAndApplyEffects(transitionId),
       }}
-      className={className}
+      className={styles.component}
     >
-      {transition.text}
-      {!transition.toPage && <div>Page de destination manquante</div>}
-      {!!incompleteCondition && <div>Condition incomplète</div>}
+      {text}
+      {errors.map(error => console.log(error) || <div className={styles.error}>{error.message}</div>)}
     </Button>
   )
 }
+
+// OLDSCHOOL
+
+// const GameTransition = ({
+//   transition,
+//   effects,
+// }) => {
+//   const { isVisible, incompleteCondition } = GameService.checkTransitionVisibility(transition)
+//   if (!isVisible) return null
+
+//   const onClick = () =>
+//     transition.toPage && GameService.changePageAndApplyEffects(transition.toPage, effects)
+//   const className = classnames(styles.component, {
+//    [styles.disabled]: transition.toPage === null || incompleteCondition,
+//   })
+
+//   return (
+//     <Button
+//       domProps={{
+//         onClick,
+//       }}
+//       className={className}
+//     >
+//       {transition.text}
+//       {!transition.toPage && <div>Page de destination manquante</div>}
+//       {!!incompleteCondition && <div>Condition incomplète</div>}
+//     </Button>
+//   )
+// }
 
 export default GameTransition
 
