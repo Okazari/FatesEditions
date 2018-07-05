9be2be10445352cc5a535e747480f2a308629920
RQM- <benoit.jacques.felix@gmail.com>
2018-07-05 11:02:24 +0200

    🔥 remove GameContainer.js

--- ui/src/components/Game/GameContainer.js
+++
@@ -1,9 +0,0 @@
-// import { connect } from 'react-redux'
-// import Game from './Game'
-
-// const mapStateToProps = state => ({
-//   pages: state.game.book.entities.page,
-//   currentPageId: state.game.currentPageId,
-// })
-
-// export default connect(mapStateToProps)(Game)
