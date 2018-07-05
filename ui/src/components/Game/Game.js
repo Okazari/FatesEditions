f1bca05a959e654f3796a2b12c79c3ba10d03392
RQM- <benoit.jacques.felix@gmail.com>
2018-07-05 11:01:13 +0200

    👌 delete old hoverTransition code

--- ui/src/components/Game/Game.js
+++ ui/src/components/Game/Game.js
@@ -3,35 +3,11 @@ import styles from './styles.scss'
 import GamePage from './GamePage'
 // import GameTree from './GameTree'
 
-// TODO: Refactor this Statefull comp to a Recompose HOC Structure
-class Game extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hoverNode: null,
-    }
-  }
-
-  hoverTransition = (hoverNode) => {
-    this.setState({ hoverNode })
-  }
-
-  outTransition = () => {
-    this.setState({ hoverNode: null })
-  }
-
-  render() {
-    return (
-      <div className={styles.wrapper}>
-        <div className={styles.content}>
-          <GamePage
-            hoverTransition={this.hoverTransition}
-            outTransition={this.outTransition}
-          />
-        </div>
-      </div>
-    )
-  }
-}
+const Game = () => (
+  <div className={styles.wrapper}>
+    <div className={styles.content}>
+      <GamePage />
+    </div>
+  </div>)
 
 export default Game
