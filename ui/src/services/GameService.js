b22c7561a58cc9c90d2f5741dae2c514d5a15a27
RQM- <benoit.jacques.felix@gmail.com>
2018-07-09 15:24:55 +0200

    ♻️ move the no destination page error into the GameService

--- ui/src/services/GameService.js
+++ ui/src/services/GameService.js
@@ -23,17 +23,17 @@ const evaluateCondition = (game, condition) => {
   return null
 }
 
-
 const checkTransitionVisibility = (game, transition) => {
   const { conditionOperator, conditions } = transition
   const { sumConditions, defaultBool } = EffectService.conditionOperator[conditionOperator]
   return conditions.reduce((acc, condition) =>
-    sumConditions(acc, evaluateCondition(game, condition)), defaultBool)
+  sumConditions(acc, evaluateCondition(game, condition)), defaultBool)
 }
 
 const applyStatEffect = (game, effect) => {
   const { stats } = game
   const { type, subject, operator, value } = effect
+  if (!type || !subject || !operator || !value) throw new Error('Effet incomplet!')
   return {
     ...game,
     stats: {
@@ -46,6 +46,7 @@ const applyStatEffect = (game, effect) => {
 const applyItemEffect = (game, effect) => {
   const { objects } = game
   const { type, subject, operator } = effect
+  if (!type || !subject || !operator) throw new Error('Effet incomplet!')
   return {
     ...game,
     objects: EffectService.effect[type][operator].exec(subject, objects),
@@ -64,20 +65,26 @@ const applyEffect = (game, effect) => {
 
 const applyEffects = (game, effects) =>
   effects.reduce((updatedGame, effect) => applyEffect(updatedGame, effect), game)
-  
-const getTransitionEffects = (game, transitionId) => game.book.transition[transitionId].effects.map(id => game.book.effect[id])
-const applyTransitionEffects = (game, transitionId) => applyEffects(game, getTransitionEffects(game, transitionId))
 
-const getPageEffects = (game, pageId) => game.book.page[pageId].effects.map(id => game.book.effect[id])
+const getTransitionEffects = (game, transitionId) =>
+  game.book.transition[transitionId].effects.map(id => game.book.effect[id])
+const applyTransitionEffects = (game, transitionId) =>
+  applyEffects(game, getTransitionEffects(game, transitionId))
+
+const getPageEffects = (game, pageId) =>
+  game.book.page[pageId].effects.map(id => game.book.effect[id])
 const applyPageEffects = (game, pageId) => applyEffects(game, getPageEffects(game, pageId))
-  
-const getDestination = (game, transitionId) => game.book.transition[transitionId].toPage
+
+const getDestination = (game, transitionId) => {
+  if (!game.book.transition[transitionId].toPage) throw new Error('Page de destination manquante')
+  return game.book.transition[transitionId].toPage
+}
 const changePage = (game, transitionId) => ({
   ...game,
   currentPageId: getDestination(game, transitionId),
 })
 
-const easier = (game) => ({
+const easier = game => ({
   get: () => game,
   applyTransitionEffects: transitionId => easier(applyTransitionEffects(game, transitionId)),
   changePage: transitionId => easier(changePage(game, transitionId)),
@@ -90,7 +97,30 @@ const changePageAndApplyEffects = (game, transitionId) => easier(game)
   .applyDestinationPageEffects()
   .get()
 
+const checkStatEffect = (effect) => {
+  const { type, subject, operator, value } = effect
+  if (!type || !subject || !operator || !value) throw new Error('Effet de stat incomplet')
+}
+
+const checkItemEffect = (effect) => {
+  const { type, subject, operator } = effect
+  if (!type || !subject || !operator) throw new Error('Effet d\'objet incomplet')
+}
+
+const checkEffect = (effect) => {
+  const { type } = effect
+  if (type === 'stat') { return checkStatEffect(effect) }
+  if (type === 'object') { return checkItemEffect(effect) }
+  return null
+}
+
+const checkEffects = (game, transitionId) => {
+  getTransitionEffects(game, transitionId).forEach(checkEffect)
+  getPageEffects(game, getDestination(game, transitionId)).forEach(checkEffect)
+}
+
 export default {
   checkTransitionVisibility,
   changePageAndApplyEffects,
+  checkEffects,
 }
