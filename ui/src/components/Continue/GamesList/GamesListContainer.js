6db7f891c79db81a056a72850910a2c15ef613e8
RQM- <benoit.jacques.felix@gmail.com>
2018-07-11 10:55:21 +0200

    🎨 fixing all the linter errors/warnings

--- ui/src/components/Continue/GamesList/GamesListContainer.js
+++ ui/src/components/Continue/GamesList/GamesListContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Query, Mutation } from 'react-apollo'
+import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import { AuthService } from 'services'
 import GamesList from './GamesList'
@@ -33,20 +33,20 @@ export const query = gql`
 
 // TODO: Add loader and error display
 const GamesListContainer = () => (
-  <Query 
+  <Query
     query={query}
     variables={{
       playerId: AuthService.getConnectedUserId(),
     }}
     fetchPolicy={'network-only'}
   >
-  {
-    ({ loading, error, data }) => {
-      if (loading) return null
-      if (error) return null
-      return <GamesList gamesList={data.author.games} />
-    } 
-  }
+    {
+      ({ loading, error, data }) => {
+        if (loading) return null
+        if (error) return null
+        return <GamesList gamesList={data.author.games} />
+      }
+    }
   </Query>
 )
 
