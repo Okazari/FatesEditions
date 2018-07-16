f02212649fcebca664d76b5d2ac84005f7e194f8
RQM- <benoit.jacques.felix@gmail.com>
2018-07-16 18:19:54 +0200

    💄 add Stats Description on hover for MVP :)

--- ui/src/components/Layout/Toolbars/common/DisplayStats/Stats/StatsContainer.js
+++ ui/src/components/Layout/Toolbars/common/DisplayStats/Stats/StatsContainer.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import Stats from './Stats'
 
 const mapStateToProps = ({ game }, { id, value }) => ({
-  name: game.book.stat[id].name,
+  stat: game.book.stat[id],
   value,
 })
 
