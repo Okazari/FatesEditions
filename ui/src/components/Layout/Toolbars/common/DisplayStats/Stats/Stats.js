0bfa879dfd657fd3322446b86149e81065b10eba
RQM- <benoit.jacques.felix@gmail.com>
2018-07-24 10:53:58 +0200

    🐛 now destructuring in Stats ( DisplayStats )

--- ui/src/components/Layout/Toolbars/common/DisplayStats/Stats/Stats.js
+++ ui/src/components/Layout/Toolbars/common/DisplayStats/Stats/Stats.js
@@ -2,11 +2,14 @@ import React from 'react'
 import styles from './style.scss'
 
 const Stats = ({ stat, value }) => {
+  const { name, description } = stat
   return (
     <div className={styles.component}>
-      <div className={styles.name}>{stat.name}</div>
+      <div className={styles.name}>{name}</div>
       <div>{value}</div>
-      <div className={styles.description}>{stat.description}</div>
+      { !!description &&
+        <div className={styles.description}>{description}</div>
+      }
     </div>
   )
 }
