729023492c9911a1dec52da55362650d549d214b
RQM- <benoit.jacques.felix@gmail.com>
2018-07-24 11:28:49 +0200

    💄 add items info tooltips display

--- ui/src/components/Layout/Toolbars/common/DisplayObjects/Item/ItemContainer.js
+++ ui/src/components/Layout/Toolbars/common/DisplayObjects/Item/ItemContainer.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import Item from './Item'
 
 const mapStateToProps = ({ game }, { objectId }) => ({
-  name: game.book.object[objectId].name,
+  item: game.book.object[objectId],
 })
 
 export default connect(mapStateToProps)(Item)
