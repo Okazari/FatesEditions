6ecc61ab60accb6f54511911617fdb770e7889b1
RQM- <benoit.jacques.felix@gmail.com>
2018-08-31 10:51:30 +0200

    💄 tweak animations styles

--- ui/src/components/Views/Game/TransitionsList/TransitionsList.js
+++ ui/src/components/Views/Game/TransitionsList/TransitionsList.js
@@ -2,21 +2,23 @@ import React from 'react'
 import Transition from './Transition'
 import styles from './style.scss'
 
-const TransitionsList = ({ transitions }) => (
-  <div className={styles.transitionsList}>
-    {
-      !!transitions && transitions.map((transitionId, index) => {
-        const delay = 50 * index + 200
-        return (
-          <Transition
-            key={transitionId}
-            transitionId={transitionId}
-            delay={delay}
-          />
-        )
-      })
-    }
-  </div>
-)
-
+const TransitionsList = ({ transitions }) => {
+  const length = transitions.length
+  return (
+    <div className={styles.transitionsList}>
+      {
+        !!transitions && transitions.map((transitionId, index) => {
+          const delay = Math.floor(150 / length) * index + 100
+          return (
+            <Transition
+              key={transitionId}
+              transitionId={transitionId}
+              delay={delay}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
 export default TransitionsList
