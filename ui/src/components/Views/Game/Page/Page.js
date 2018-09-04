3e238fd6ec1ee573bbe18e7930b2b76e3d226b7d
RQM- <benoit.jacques.felix@gmail.com>
2018-09-04 16:56:46 +0200

    💄 add turn page animation

--- ui/src/components/Views/Game/Page/Page.js
+++ ui/src/components/Views/Game/Page/Page.js
@@ -1,27 +1,50 @@
 import React from 'react'
+import classnames from 'classnames'
 import { convertFromRaw, Editor, EditorState, ContentState } from 'draft-js'
 import Image from 'components/common/TextEditor/Image'
 import styles from './style.scss'
 
 class Page extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newPage: true,
+    }
+    this.timeOut = setTimeout(() => {
+      this.setState({ newPage: false })
+    }, 300)
+  }
+
   componentDidUpdate(prevProps) {
     const { resetScrolling } = this.props
     if (prevProps.page.id !== this.props.page.id) {
       resetScrolling()
+      console.log('pages are diff !==')
+      this.setState({ newPage: true })
+      this.timeOut = setTimeout(() => {
+        this.setState({ newPage: false })
+      }, 300)
     }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeOut)
   }
 
   render() {
     const { page = {} } = this.props
+    const { newPage } = this.state
     const pageContent = page.text ? convertFromRaw(JSON.parse(page.text)) : ContentState.createFromText('')
-
     const blockRenderer = (block) => {
       if (block.getType() === 'atomic') return { component: Image, editable: false }
       return null
     }
+    const className = classnames(styles.component, {
+      [styles.newPage]: newPage,
+    })
     return (
       <div
-        className={styles.component}
+        className={className}
         id="page"
       >
         <Editor
