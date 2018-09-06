c5cd47688217853785e5f2efba279d01770ebfee
RQM- <benoit.jacques.felix@gmail.com>
2018-09-06 11:05:50 +0200

    🚸 add forbidden comp to display an alert message on mobile and tablet in MyBooks comp to tell them book edition's not available

---
+++ ui/src/components/Views/MyBooks/Forbidden/Forbidden.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AppLayout, AlertMessage, Emphasis } from 'components/common'
+
+const tabForbidden = { label: 'Indisponible' }
+const tabs = [tabForbidden]
+
+const Forbidden = (props) => (
+  <div>
+    <AppLayout {...props} tabs={tabs}>
+    <AlertMessage title={'Edition des brouillons sur mobile et tablette impossible.'}>
+        La partie<Emphasis>édition</Emphasis>de livre n'est pas disponible sur les appareils<Emphasis>mobiles</Emphasis>et les<Emphasis>tablettes.</Emphasis>
+        Pour accéder à l'édition, veuillez vous connecter via un<Emphasis>ordinateur.</Emphasis> 
+    </AlertMessage>
+    </AppLayout>
+  </div>
+)
+
+export default Forbidden
