1c68b0847a6e4b7a7081e9335aa9c430fe313850
RQM- <benoit.jacques.felix@gmail.com>
2018-09-07 10:06:04 +0200

    💄 change style for alertMessage on Redirection

--- ui/src/components/Views/Connection/Connection.js
+++ ui/src/components/Views/Connection/Connection.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AppLayout, AlertMessage, Emphasis } from 'components/common'
 import { RouteService } from 'services'
+import styles from './style.scss'
 
 const tabSignIn = { label: 'Connexion', link: RouteService.routes.signin() }
 const tabSignUp = { label: 'Inscription', link: RouteService.routes.signup() }
@@ -8,7 +9,10 @@ const tabPassword = { label: 'mot de passe perdu?', link: RouteService.routes.pa
 const tabs = [tabSignIn, tabSignUp, tabPassword]
 
 const Message = () => (
-  <AlertMessage title={'Vous avez été redirigé'}>
+  <AlertMessage 
+    title={'Vous avez été redirigé'}
+    className={styles.alertMessage}
+  >
     Pour pouvoir<Emphasis>écrire</Emphasis>votre propre livre ou bien
     <Emphasis>jouer</Emphasis>aux livres déjà publiés,
     vous devez être<Emphasis>connecté.</Emphasis>
--- ui/src/components/common/AlertMessage/AlertMessage.js
+++ ui/src/components/common/AlertMessage/AlertMessage.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import classnames from 'classnames'
 import styles from './style.scss'
 
-const AlertMessage = ({ title, children }) => (
-  <div className={styles.alertMessage}>
-    <h4 className={styles.alertTitle}>
-      {title}
-    </h4>
-    <div>
-      {children}
+const AlertMessage = ({ title, children, className }) => {
+  const finalClassName = classnames(styles.alertMessage, className) 
+  return (
+    <div className={finalClassName}>
+      <h4 className={styles.alertTitle}>
+        {title}
+      </h4>
+      <div>
+        {children}
+      </div>
     </div>
-  </div>
-)
-
+  )
+}
 export default AlertMessage
