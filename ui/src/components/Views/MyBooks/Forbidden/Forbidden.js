a67c08a1774ed88a053eddbc5ad52e7a51a96e3b
RQM- <benoit.jacques.felix@gmail.com>
2018-09-07 14:17:52 +0200

    💄 fix Forbidden alert Message margin

--- ui/src/components/Views/MyBooks/Forbidden/Forbidden.js
+++ ui/src/components/Views/MyBooks/Forbidden/Forbidden.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AppLayout, AlertMessage, Emphasis } from 'components/common'
+import styles from './style.scss'
 
 const tabForbidden = { label: 'Indisponible' }
 const tabs = [tabForbidden]
@@ -7,7 +8,10 @@ const tabs = [tabForbidden]
 const Forbidden = props => (
   <div>
     <AppLayout {...props} tabs={tabs}>
-      <AlertMessage title={'Edition des brouillons sur mobile et tablette impossible.'}>
+      <AlertMessage
+        title={'Edition des brouillons sur mobile et tablette impossible.'}
+        className={styles.forbidden} 
+      >
         La partie<Emphasis>édition</Emphasis>de livre {"n'est"} pas disponible sur les appareils
         <Emphasis>mobiles</Emphasis>et les<Emphasis>tablettes.</Emphasis>
         Pour accéder à {"l'édition"}, veuillez vous connecter via un
