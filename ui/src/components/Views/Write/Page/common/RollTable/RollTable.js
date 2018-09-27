33403a69ccb4a659c9ab7c11564d76b332b40086
RQM- <benoit.jacques.felix@gmail.com>
2018-09-27 14:50:31 +0200

    🚑 Change animation for the AddRollButton not to be in PoseGroup

--- ui/src/components/Views/Write/Page/common/RollTable/RollTable.js
+++ ui/src/components/Views/Write/Page/common/RollTable/RollTable.js
@@ -9,6 +9,36 @@ import { dataRow } from 'styles/reactPoseAnimation'
 import styles from './style.scss'
 
 const AnimatedRollRow = posed.div(dataRow)
+const AnimatedAddRollButton = posed.div({
+  hiddenButton: {
+    height: 0,
+    opacity: 0,
+    transition: {
+      height: {
+        duration: 100,
+        delay: 100,
+      },
+      opacity: {
+        duration: 100,
+        delay: 0,
+      },
+    },
+  },
+  visibleButton: {
+    height: 'auto',
+    opacity: 1,
+    transition: {
+      height: {
+        duration: 100,
+        delay: 0,
+      },
+      opacity: {
+        duration: 200,
+        delay: 100,
+      },
+    },
+  },
+})
 
 const RollTable = ({
   book,
@@ -19,12 +49,24 @@ const RollTable = ({
 }) => {
   return (
     <div className={styles.component}>
+      <AnimatedAddRollButton
+        className={styles.animatedAddRollButton}
+        pose={rolls.length < 1 ? 'visibleButton' : 'hiddenButton'}
+      >
+        <div className={styles.centerButton}>
+          <WideButton
+            domProps={{ onClick: addRoll }}
+          >
+            Ajouter un Lancer de dé
+          </WideButton>
+        </div>
+      </AnimatedAddRollButton>
       { rolls.length > 0 &&
-        <span>Lancer de dé</span>
+        <span key="label">Lancer de dé</span>
       }
       <PoseGroup>
-        { rolls.length > 0
-          ? (rolls.map(roll => (
+        {
+          rolls.map(roll => (
             <AnimatedRollRow key={roll.id}>
               <RowWithDeleteButton removeRow={() => removeRoll(roll.id)}>
                 <RollRow
@@ -34,17 +76,7 @@ const RollTable = ({
                 />
               </RowWithDeleteButton>
             </AnimatedRollRow>
-          ))) : (
-            <AnimatedRollRow key="addRollButton">
-              <div className={styles.centerButton}>
-                <WideButton
-                  domProps={{ onClick: addRoll }}
-                >
-                  Ajouter un Lancer de dé
-                </WideButton>
-              </div>
-            </AnimatedRollRow>
-          )
+          ))
         }
       </PoseGroup>
     </div>
